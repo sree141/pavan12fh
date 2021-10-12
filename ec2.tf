00428b76d9266ee3940839130af5d9954585ac93
new srre <91159068+sree141@users.noreply.github.com>
2021-10-12 13:25:49 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region ="us-east-1"
+    access_key = "AKIAZP7U5GBOWGU66JUQ"
+    secret_key = "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"
+
+}
+resource "aws_instance" "terraform-ec3"{  
+    ami           = "ami-09e67e426f25ce0d7"  
+    instance_type = "t2.micro"
+    key_name = "basha"
+    vpc_security_group_ids = [ "sg-0039fbacfa9b3fe01" ]
+    subnet_id = "subnet-3d2e061c"
+    root_block_device {
+    
+        volume_type = "gp2"
+        volume_size = "8"
+        delete_on_termination = true
+      
+    }
+  tags =  {
+  Name = "sree"  
+  }
+}
